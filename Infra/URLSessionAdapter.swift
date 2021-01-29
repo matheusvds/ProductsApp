ff92399e55b676e8fc7b63d65c2d229e3c98af46
Matheus Sousa <matheusvds@gmail.com>
2021-01-29 18:35:23 +0000

    implements the http client

---
+++ Infra/URLSessionAdapter.swift
@@ -0,0 +1,40 @@
+import Foundation
+import Data
+
+public final class URLSessionAdapter: HttpClient {
+
+    private let session: URLSession
+
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    public func send(from request: URLRequest, completion: @escaping (HttpClient.Result) -> Void) {
+        
+        session.dataTask(with: request) { (data, urlResponse, error) in
+            if let data = data, let response = urlResponse as? HTTPURLResponse, error == nil {
+                switch response.statusCode {
+                case 204:
+                    return completion(.success(nil))
+                case 200...299:
+                    return completion(.success(data))
+                case 401:
+                    return completion(.failure(.unauthorized))
+                case 403:
+                    return completion(.failure(.forbidden))
+                case 400...499:
+                    return completion(.failure(.badRequest))
+                case 500...599:
+                    return completion(.failure(.serverError))
+                default:
+                    return completion(.failure(.noConnection))
+                }
+            } else {
+                return completion(.failure(.noConnection))
+            }
+        }.resume()
+    }
+    
+
+}
+
