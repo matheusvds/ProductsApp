b18d8fa97282259cb1f20b6eb1a6297e7e2775e6
Matheus Sousa <matheusvds@gmail.com>
2021-01-29 17:30:29 +0000

    test if get products is completing its callback after deallocation

--- DataTests/RemoteGetProductsTests.swift
+++ DataTests/RemoteGetProductsTests.swift
@@ -4,4 +4,17 @@ import Domain
 
 class RemoteGetProductsTests: XCTestCase {
     
+    func test_get_products_should_not_complete_when_sut_is_deallocated() {
+        let httpSpy = HttpClientSpy()
+        var sut: RemoteGetProducts? = RemoteGetProducts(httpClient: httpSpy)
+        var receivedResult: Result<Products, DomainError>?
+        
+        sut?.get() { receivedResult = $0 }
+        sut = nil
+        
+        httpSpy.complete(withError: .unauthorized)
+        
+        XCTAssertNil(receivedResult)
+    }
+    
 }
